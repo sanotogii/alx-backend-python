3036712c7b151c4a8a53c097ee4f77ccfc5538b5
sanotogii <nasaereh@gmail.com>
2024-08-05 13:31:54 +0200

    task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+convert a float to a string.
+"""
+
+def to_str(n: float) -> str:
+    """
+    Convert a float to its string representation.
+    """
+    return str(n)
+
