5074fa31005ca1498af57375bc910bbf5bef1720
sanotogii <nasaereh@gmail.com>
2024-08-05 14:06:19 +0200

    adv task

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+This module contains a function safely_get_value that retrieves
+a value from a dictionary safely, with a default fallback.
+"""
+from typing import Mapping, Any, Union, TypeVar
+T = TypeVar('T')
+
+
+def safe_first_element(
+    dct: Mapping[Any, T], key: Any, default: Union[T, None] = None
+) -> Union[T, None]:
+    """
+    Retrieve a value from a dictionary safely with a default fallback.
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
