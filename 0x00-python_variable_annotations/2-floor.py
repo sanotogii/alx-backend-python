773350adecc567614353799849c12e4e0f7a84e9
sanotogii <nasaereh@gmail.com>
2024-08-05 12:51:39 +0200

    task 3

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
+import math
 """
 Basic annotations - floor
 """
-
+ 
 
 def floor(n: float) -> int:
     """
     floor function
     """
-    return n
+    return math.floor(n)
