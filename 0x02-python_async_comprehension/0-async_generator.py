300e3e93d307ec98a1f7367a722c3e895a8129de
sanotogii <nasaereh@gmail.com>
2024-08-06 22:30:54 +0200

    task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -12,6 +12,6 @@ async def async_generator():
     wait 1 second,
     then yield a random number between 0 and 10
     """
-    for _ in range(0, 10):
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
